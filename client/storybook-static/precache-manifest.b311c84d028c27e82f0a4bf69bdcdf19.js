6203ea9adf6b0d1af9121ce89ca9f1e1f3adac74
LiubavaL <liuba.vitko@gmail.com>
2020-11-26 22:55:33 +0300

    modified chromatic.yaml

---
+++ client/storybook-static/precache-manifest.b311c84d028c27e82f0a4bf69bdcdf19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3cd458da1e7cba185159d730e45f349",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ad98915ae737585974a9.bundle.js"
+  },
+  {
+    "url": "runtime~main.ad98915ae737585974a9.bundle.js"
+  },
+  {
+    "revision": "1a8affc99c261f06e19f",
+    "url": "static/css/main.d38eb39c.chunk.css"
+  },
+  {
+    "url": "vendors~main.ad98915ae737585974a9.bundle.js"
+  },
+  {
+    "url": "vendors~main.ad98915ae737585974a9.bundle.js.LICENSE.txt"
+  }
+]);
